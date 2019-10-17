f281995ff7934450545b9962ad669910e6f16fb7
Faranio <40353497+Faranio@users.noreply.github.com>
2019-10-17 14:22:03 +0600

    Add SQLException

--- src/main/java/backend/MainApplication.java
+++ src/main/java/backend/MainApplication.java
@@ -8,20 +8,28 @@ import javax.ws.rs.core.Application;
 
 @ApplicationPath("/services")
 public class MainApplication extends Application {
+    
     private Set<Object> singletons = new HashSet<Object>();
     private Set<Class<?>> empty = new HashSet<Class<?>>();
     
-    public MainApplication() {
+    public MainApplication() throws SQLException {
+        
         singletons.add(new ListRoutesService());
+        
     }
     
     @Override
     public Set<Class<?>> getClasses() {
+        
         return empty;
+        
     }
 
     @Override
     public Set<Object> getSingletons() {
+        
         return singletons;
+        
     }
+    
 }
