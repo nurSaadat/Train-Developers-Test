02f056a99f2b2b04695f0a06564fb5359756717f
Faranio <agent006_99@mail.ru>
2019-10-30 13:22:00 +0600

    Modified sign-up backend

--- src/main/java/backend/SignUpService.java
+++ src/main/java/backend/SignUpService.java
@@ -4,9 +4,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 @Path("/signup")
@@ -18,73 +20,53 @@ public class SignUpService {
 	private static final String LNAME_PATTERN = "[A-Z]+([ '-][a-zA-Z]+)*";
 	
 	@POST
-    public Response register(@FormParam("data") List<String> data) throws SQLException, ClassNotFoundException {
+    public Response register(@FormParam("email") String email, @FormParam("fname") String fname, @FormParam("lname") String lname, @FormParam("password") String password) throws SQLException, ClassNotFoundException {
 		
 		MySQLConnector db = new MySQLConnector(3306, "RailwayStation", "user", "Password123!");
 		
-		for (int i = 0; i < data.size(); i++) {
+		if (email == null || fname == null || lname == null || password == null || email.compareTo("") == 0 || fname.compareTo("") == 0 || lname.compareTo("") == 0 || password.compareTo("") == 0) {
 			
-			if (data.get(i).length() == 0) {
-				
-				return Response.serverError().entity("Error! One of the fields is empty!").build();
-				
-			}
+			return Response.serverError().entity("Error! One of the fields is empty!").build();
 			
-			if (i == 0) {
+		}
 				
-				ResultSet rs = db.getData("SELECT Email FROM User;");
-				
-				while (rs.next()) {
-					
-					if (rs.getString("Email").toLowerCase().compareTo(data.get(0).toLowerCase()) == 0) {
-						
-						return Response.serverError().entity("Error! That email address is already present in the database!").build();
-						
-					}
-					
-				}
-				
-				if (!data.get(0).matches(EMAIL_PATTERN)) {
-					
-					return Response.serverError().entity("Error! Email provided is invalid!").build();
-					
-				}
-				
-			}
+		ResultSet rs = db.getData("SELECT Email FROM User WHERE Email = '" + email + "';");
+		
+		while (rs.next()) {
 			
-			if (i == 1) {
+			if (rs.getString("Email").toLowerCase().compareTo(email.toLowerCase()) == 0) {
 				
-				if (!data.get(i).matches(FNAME_PATTERN)) {
-					
-					return Response.serverError().entity("Error! First name provided is invalid!").build();
-					
-				}
-				
-			}
-			
-			if (i == 2) {
-				
-				if (!data.get(i).matches(LNAME_PATTERN)) {
-					
-					return Response.serverError().entity("Error! Last name provided is invalid!").build();
-					
-				}
-				
-			}
-			
-			if (i == 3) {
-				
-				if (data.get(3).length() < 6 || !data.get(3).matches(PASSWORD_PATTERN)) {
-					
-					return Response.serverError().entity("Error! Password should have between 6 and 40 characters. It should contain at least 1 digit, 1 lowercase and 1 uppercase character! Allowed symbols, from which at least 1 should be present: @#$%!").build();
-					
-				}
+				return Response.serverError().entity("Error! That email address is already present in the database!").build();
 				
 			}
 			
 		}
 		
-		db.insertData("INSERT INTO User (Email, FName, LName, Password) VALUES (" + data.get(0) + ", " + data.get(1) + ", " + data.get(2) + ", " + data.get(3) + ");");
+		if (!email.matches(EMAIL_PATTERN)) {
+			
+			return Response.serverError().entity("Error! Email provided is invalid!").build();
+			
+		}
+				
+		if (!fname.matches(FNAME_PATTERN)) {
+			
+			return Response.serverError().entity("Error! First name provided is invalid!").build();
+			
+		}
+				
+		if (!lname.matches(LNAME_PATTERN)) {
+			
+			return Response.serverError().entity("Error! Last name provided is invalid!").build();
+			
+		}
+				
+		if (!password.matches(PASSWORD_PATTERN)) {
+			
+			return Response.serverError().entity("Error! Password should have between 6 and 40 characters. It should contain at least 1 digit, 1 lowercase and 1 uppercase character! Allowed symbols, from which at least 1 should be present: @#$%!").build();
+			
+		}
+		
+		db.insertData("INSERT INTO User (Email, FName, LName, Password) VALUES (" + email + ", " + fname + ", " + lname + ", " + password + ");");
     	
         return Response.ok().build();
         
